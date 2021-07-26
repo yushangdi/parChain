da3da03d2629522db9c77bff1e8ecf3f98dbfa54
shangdiy <sy543@cornell.edu>
2021-07-26 06:54:52 -0400

    cpython

---
+++ cpython/cpython.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+[ -d "outputs" ] || mkdir "outputs"
+[ -d "outputs/outputs_cscipy" ] || mkdir "outputs/outputs_cscipy"
+[ -d "outputs/outputs_csklearn" ] || mkdir "outputs/outputs_csklearn"
+
+
+
+datasets=(
+    "10D_UCI1_19K" 
+    "2D_GaussianDisc_10K"
+    "10D_UCI4_100K"
+)      
+#    
+dims=(10 2 10)
+sizes=(19020 10000 100000)
+methods={"complete", "ward", "avg", "avgsq"}
+libraries={"scipy", "sklearn"}
+
+ind=0
+
+
+for dataset in "${datasets[@]}"; do
+  for method in "${methods[@]}"; do
+    for library in "${libraries[@]}"; do
+      echo "./cpython datasets/${dataset}.pbbs ${sizes[$ind]} ${dims[$ind]} 1 ${method} ${library}  >> outputs/outputs_c${library}/c${library}_${dataset}_1th.txt"
+      echo ${dataset}_${method} >> outputs/outputs_c${library}/c${library}_${dataset}_1th.txt
+      ./cpython datasets/${dataset}.pbbs ${sizes[$ind]} ${dims[$ind]} 1 ${method} ${library}>> outputs/outputs_c${library}/c${library}_${dataset}_1th.txt
+    done
+  done
+  let ind++
+done
+
+echo "done"
